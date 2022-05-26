ce04ed85b34647169d07fedfb42854dd4c96c131
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-05-26 15:21:54 +0300

    Mail merge logic added.

---
+++ Mail Merge Project Start/main.py
@@ -0,0 +1,15 @@
+names = []
+letters = []
+all_letters = []
+with open("Input/Names/invited_names.txt", "r") as invited_names_file:
+    for name in invited_names_file.readlines():
+        i = name.replace("\n", "")
+        names.append(i)
+
+
+for name in names:
+    with open("Input/Letters/starting_letter.txt", "r") as letter_folder:
+        i = letter_folder.readline().replace("[name]", name)
+        with open(f'Output/ReadyToSend/{name}_letter', 'w') as f:
+            f.write(i + letter_folder.read())
+
